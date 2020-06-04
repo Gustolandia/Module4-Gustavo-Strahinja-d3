7aa80d396fe3ec72484bb8d0e27b758acb75691f
LaLe1993 <63840101+LaLe1993@users.noreply.github.com>
2020-06-04 15:41:45 +0200

    BookList

--- books/src/App.js
+++ books/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AlertDanger from "./components/alert";
 import SingleBook from "./components/SingleBook";
 import MyBadge from './components/MyBadge';
+import BookList from './components/BookList'
 
 let books = require("./components/data/fantasy.json");
 console.log(books);
@@ -12,6 +13,7 @@ function App() {
       <SingleBook book={books[0]}/>
       <AlertDanger text="This is the warning"/>
       <MyBadge text='My Badge text' color="primary" />
+      <BookList books={books}/>
    </>
   );
 }
